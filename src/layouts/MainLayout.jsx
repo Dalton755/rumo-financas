662be5561eb7f1ffc9e202ee54f32a5eabb362e5
Dalton Silva <rochadalton00@gmail.com>
2026-06-12 16:52:19 -0300

    Melhora logo do Rumo

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,62 +1,69 @@
-import { Link } from 'react-router-dom'
+  import { Link } from 'react-router-dom'
 
-function MainLayout({ children }) {
+  function MainLayout({ children }) {
 
-  return (
-    <div className="container-fluid">
+    return (
+      <div className="container-fluid">
 
-      <div className="row">
+        <div className="row">
 
-        <div
-          className="col-md-2 bg-dark text-white min-vh-100 p-3"
-        >
+          <div
+            className="col-md-2 bg-dark text-white min-vh-100 p-3"
+          >
 
-          <h3>Rumo</h3>
+            <div className="text-center mb-4">
 
-          <p>
-            Seu futuro financeiro
-          </p>
+    <h2 className="fw-bold text-success">
+      ↗ Rumo
+    </h2>
 
-          <hr />
+    <small className="text-light">
+      Veja para onde seu dinheiro
+      está levando você
+    </small>
 
-          <div className="d-grid gap-2">
+  </div>
 
-            <Link
-              to="/dashboard"
-              className="btn btn-outline-light"
-            >
-              Dashboard
-            </Link>
+            <hr />
 
-            <Link
-              to="/contas"
-              className="btn btn-outline-light"
-            >
-              Contas
-            </Link>
+            <div className="d-grid gap-2">
 
-            <Link
-              to="/movimentacoes"
-              className="btn btn-outline-light"
-            >
-              Movimentações
-            </Link>
+              <Link
+                to="/dashboard"
+                className="btn btn-outline-light"
+              >
+                Dashboard
+              </Link>
+
+              <Link
+                to="/contas"
+                className="btn btn-outline-light"
+              >
+                Contas
+              </Link>
+
+              <Link
+                to="/movimentacoes"
+                className="btn btn-outline-light"
+              >
+                Movimentações
+              </Link>
+
+            </div>
+
+          </div>
+
+          <div className="col-md-10 p-4">
+
+            {children}
 
           </div>
 
         </div>
 
-        <div className="col-md-10 p-4">
-
-          {children}
-
-        </div>
-
       </div>
+    )
 
-    </div>
-  )
+  }
 
-}
-
-export default MainLayout
+  export default MainLayout
