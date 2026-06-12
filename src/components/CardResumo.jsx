071f3197c6aab1e788874dcdb2ed94beb2725617
Dalton Silva <dalton.silva.app@gmail.com>
2026-06-12 15:45:59 -0300

    Configura roteamento da Vercel

--- src/components/CardResumo.jsx
+++ src/components/CardResumo.jsx
@@ -1,32 +1,28 @@
 function CardResumo({
-    titulo,
-    valor
-  }) {
-  
-    return (
-  
-      <div className="col-md-3">
-  
-        <div className="card shadow-sm">
-  
-          <div className="card-body">
-  
-            <h6 className="text-muted">
-              {titulo}
-            </h6>
-  
-            <h3>
-              {valor}
-            </h3>
-  
-          </div>
-  
+  titulo,
+  valor
+}) {
+  return (
+    <div className="col-md-3 mb-3">
+
+      <div className="card shadow border-0 h-100">
+
+        <div className="card-body">
+
+          <h6 className="text-muted">
+            {titulo}
+          </h6>
+
+          <h3 className="fw-bold">
+            {valor}
+          </h3>
+
         </div>
-  
+
       </div>
-  
-    )
-  
-  }
-  
-  export default CardResumo
+
+    </div>
+  )
+}
+
+export default CardResumo
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,108 +1,113 @@
-    import { useEffect, useState } from 'react'
-    import { supabase } from '../services/supabase'
-    import MainLayout from '../layouts/MainLayout'
+        import { useEffect, useState } from 'react'
+        import { supabase } from '../services/supabase'
+        import MainLayout from '../layouts/MainLayout'
+        import CardResumo from '../components/CardResumo'
 
-    function Dashboard() {
+        function Dashboard() {
 
-    const [usuario, setUsuario] = useState(null)
-    const [dashboard, setDashboard] = useState(null)
+        const [usuario, setUsuario] = useState(null)
+        const [dashboard, setDashboard] = useState(null)
 
-    useEffect(() => {
+        useEffect(() => {
 
-        async function carregarUsuario() {
+            async function carregarUsuario() {
 
-        const {
-            data: { user }
-        } = await supabase.auth.getUser()
+            const {
+                data: { user }
+            } = await supabase.auth.getUser()
 
-        setUsuario(user)
+            setUsuario(user)
 
-        if (user) {
-            carregarDashboard(user.id)
-        }
-
-        }
-
-        carregarUsuario()
-
-        async function carregarDashboard(userId) {
-
-            const { data, error } = await supabase
-            .from('vw_dashboard')
-            .select('*')
-            .eq('usuario_id', userId)
-            .single()
-        
-            console.log('Dashboard:', data)
-            console.log('Erro Dashboard:', error)
-        
-            if (!error) {
-            setDashboard(data)
+            if (user) {
+                carregarDashboard(user.id)
             }
+
+            }
+
+            carregarUsuario()
+
+            async function carregarDashboard(userId) {
+
+                const { data, error } = await supabase
+                .from('vw_dashboard')
+                .select('*')
+                .eq('usuario_id', userId)
+                .single()
+            
+                console.log('Dashboard:', data)
+                console.log('Erro Dashboard:', error)
+            
+                if (!error) {
+                setDashboard(data)
+                }
+            
+            }
+
+        }, [])
+
+        async function sair() {
+
+            await supabase.auth.signOut()
+
+            window.location.href = '/'
+
+        }
+
+        return (
+            <MainLayout>
+
+            <h1>Rumo</h1>
+
+            <p>
+                Veja para onde seu dinheiro está levando você.
+            </p>
+
+            <hr />
+
+            <h2>
+                Olá,
+                {' '}
+                {usuario?.email}
+            </h2>
+
+            <br />
+
+            <hr />
+
+            <div className="row">
+
+    <CardResumo
+        titulo="Saldo Atual"
+        valor={`R$ ${dashboard?.saldo_total ?? 0}`}
+    />
+
+    <CardResumo
+        titulo="Receitas do Mês"
+        valor={`R$ ${dashboard?.receitas_mes ?? 0}`}
+    />
+
+    <CardResumo
+        titulo="Despesas do Mês"
+        valor={`R$ ${dashboard?.despesas_mes ?? 0}`}
+    />
+
+    <CardResumo
+        titulo="Índice de Rumo"
+        valor={dashboard?.indice_rumo ?? 0}
+    />
+
+    </div>
+
+        <hr />
+
+            <button onClick={sair}>
+                Sair
+            </button>
+
+            </MainLayout>
+    )
+
         
         }
 
-    }, [])
-
-    async function sair() {
-
-        await supabase.auth.signOut()
-
-        window.location.href = '/'
-
-    }
-
-    return (
-        <MainLayout>
-
-        <h1>Rumo</h1>
-
-        <p>
-            Veja para onde seu dinheiro está levando você.
-        </p>
-
-        <hr />
-
-        <h2>
-            Olá,
-            {' '}
-            {usuario?.email}
-        </h2>
-
-        <br />
-
-        <hr />
-
-    <h3>Saldo Atual</h3>
-    <p>
-    R$ {dashboard?.saldo_total ?? 0}
-    </p>
-
-    <h3>Receitas do Mês</h3>
-    <p>
-    R$ {dashboard?.receitas_mes ?? 0}
-    </p>
-
-    <h3>Despesas do Mês</h3>
-    <p>
-    R$ {dashboard?.despesas_mes ?? 0}
-    </p>
-
-    <h3>Índice de Rumo</h3>
-    <p>
-    {dashboard?.indice_rumo ?? 0}
-    </p>
-
-    <hr />
-
-        <button onClick={sair}>
-            Sair
-        </button>
-
-        </MainLayout>
-)
-
-    
-    }
-
-    export default Dashboard
+        export default Dashboard
